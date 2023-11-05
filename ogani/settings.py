fbed171386503d1b22d3e5636db3c0949b253f19
Mushfikur Rahman <batman.exe@hotmail.com>
2023-11-05 14:58:37 +0600

    Configured templates and initialized apps

--- ogani/settings.py
+++ ogani/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
+    'blog',
+    'contact',
+    'account',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'ogani.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+# Configuring Static Files
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static' 
+STATICFILES_DIRS = [
+    'ogani/static', 
+]
+
+
+# Configuring Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def shop(request):
+    return render(request, "shop.html")
